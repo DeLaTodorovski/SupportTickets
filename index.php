280946ffb561fd242435d01447a86a0018094413
DeLaTodorovski <daniel.likovno@gmail.com>
2024-05-14 15:08:05 +0200

    Added login page and style

--- index.php
+++ index.php
@@ -41,6 +41,21 @@ $tickets = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		<?php endforeach; ?>
 	</div>
 
+    <div class="login">
+			<h1>Login</h1>
+			<form action="authenticate.php" method="post">
+				<label for="username">
+					<i class="fas fa-user"></i>
+				</label>
+				<input type="text" name="username" placeholder="Username" id="username" required>
+				<label for="password">
+					<i class="fas fa-lock"></i>
+				</label>
+				<input type="password" name="password" placeholder="Password" id="password" required>
+				<input type="submit" value="Login">
+			</form>
+		</div>
+
 </div>
 
 <?=template_footer()?>
